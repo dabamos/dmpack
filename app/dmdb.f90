fa5c28505ee4996263c06c62cf66008f039aebda
Philipp <pengel@hs-nb.de>
2023-12-13 22:59:03 +0100

    Updated observation forwarding.

--- app/dmdb.f90
+++ app/dmdb.f90
@@ -89,6 +89,80 @@ program dmdb
     if (dm_is_error(rc)) call halt(1)
     call halt(0)
 contains
+    integer function forward_observ(observ, name) result(rc)
+        !! Forwards given observation to next receiver.
+        type(observ_type), intent(inout)        :: observ !! Observation to forward.
+        character(len=*),  intent(in), optional :: name   !! App name.
+
+        integer           :: next
+        type(mqueue_type) :: mqueue
+
+        rc   = E_NONE
+        next = observ%next
+
+        do
+            ! Increase the receiver index.
+            next = max(0, next) + 1
+
+            ! End of receiver list reached?
+            if (next > observ%nreceivers) then
+                call dm_log(LOG_DEBUG, 'no receivers left in observ ' // observ%name, observ=observ)
+                return
+            end if
+
+            ! Invalid receiver name?
+            if (.not. dm_id_valid(observ%receivers(next))) then
+                rc = E_INVALID
+                call dm_log(LOG_ERROR, 'invalid receiver ' // trim(observ%receivers(next)) // &
+                            ' in observ ' // observ%name, observ=observ, error=rc)
+                return
+            end if
+
+            ! Cycle to next + 1 if receiver name equals app name. We don't want
+            ! to send the observation to this program instance.
+            if (.not. present(name)) exit
+            if (observ%receivers(next) /= name) exit
+            call dm_log(LOG_DEBUG, 'skipping receiver ' // dm_itoa(next) // ' (' // &
+                        trim(observ%receivers(next)) // ') of observ ' // observ%name)
+        end do
+
+        mqueue_block: block
+            ! Open message queue of receiver for writing.
+            rc = dm_mqueue_open(mqueue   = mqueue, &
+                                type     = TYPE_OBSERV, &
+                                name     = observ%receivers(next), &
+                                access   = MQUEUE_WRONLY, &
+                                blocking = APP_MQ_BLOCKING)
+
+            if (dm_is_error(rc)) then
+                call dm_log(LOG_ERROR, 'failed to open mqueue /' // observ%receivers(next), &
+                            observ=observ, error=rc)
+                exit mqueue_block
+            end if
+
+            ! Send observation to message queue.
+            observ%next = next
+            rc = dm_mqueue_write(mqueue, observ)
+
+            if (dm_is_error(rc)) then
+                call dm_log(LOG_ERROR, 'failed to send observ ' // trim(observ%name) // &
+                            ' to mqueue /' // observ%receivers(next), observ=observ, error=rc)
+                exit mqueue_block
+            end if
+
+            call dm_log(LOG_DEBUG, 'sent observ ' // trim(observ%name) // ' to mqueue /' // &
+                        observ%receivers(next), observ=observ)
+        end block mqueue_block
+
+        ! Close message queue.
+        rc = dm_mqueue_close(mqueue)
+
+        if (dm_is_error(rc)) then
+            call dm_log(LOG_WARNING, 'failed to close mqueue /' // observ%receivers(next), &
+                        observ=observ, error=rc)
+        end if
+    end function forward_observ
+
     integer function read_args(app) result(rc)
         !! Reads command-line arguments and settings from configuration file.
         type(app_type), intent(inout) :: app
@@ -179,65 +253,6 @@ contains
         call dm_config_close(config)
     end function read_config
 
-    integer function send_observ(observ) result(rc)
-        type(observ_type), intent(inout) :: observ
-
-        integer           :: next
-        type(mqueue_type) :: mqueue
-
-        next = max(0, observ%next) + 1
-
-        ! Validate receiver.
-        if (next > observ%nreceivers) then
-            call dm_log(LOG_DEBUG, 'no receivers left in observ ' // observ%name, observ=observ)
-            rc = E_NONE
-            return
-        end if
-
-        if (.not. dm_id_valid(observ%receivers(next))) then
-            call dm_log(LOG_ERROR, 'invalid receiver ' // trim(observ%receivers(next)) // &
-                        ' in observ ' // observ%name, observ=observ, error=E_INVALID)
-            rc = E_INVALID
-            return
-        end if
-
-        mqueue_block: block
-            ! Open message queue of receiver for writing.
-            rc = dm_mqueue_open(mqueue   = mqueue, &
-                                type     = TYPE_OBSERV, &
-                                name     = observ%receivers(next), &
-                                access   = MQUEUE_WRONLY, &
-                                blocking = APP_MQ_BLOCKING)
-
-            if (dm_is_error(rc)) then
-                call dm_log(LOG_ERROR, 'failed to open mqueue /' // observ%receivers(next), &
-                            observ=observ, error=rc)
-                exit mqueue_block
-            end if
-
-            ! Send observation to message queue.
-            observ%next = next
-            rc = dm_mqueue_write(mqueue, observ)
-
-            if (dm_is_error(rc)) then
-                call dm_log(LOG_ERROR, 'failed to send observ ' // trim(observ%name) // &
-                            ' to mqueue /' // observ%receivers(next), observ=observ, error=rc)
-                exit mqueue_block
-            end if
-
-            call dm_log(LOG_DEBUG, 'sent observ ' // trim(observ%name) // ' to mqueue /' // &
-                        observ%receivers(next), observ=observ)
-        end block mqueue_block
-
-        ! Close message queue.
-        rc = dm_mqueue_close(mqueue)
-
-        if (dm_is_error(rc)) then
-            call dm_log(LOG_WARNING, 'failed to close mqueue /' // observ%receivers(next), &
-                        observ=observ, error=rc)
-        end if
-    end function send_observ
-
     subroutine halt(stat)
         !! Cleans up and stops program.
         integer, intent(in) :: stat
@@ -319,7 +334,7 @@ contains
             end do db_loop
 
             ! Forward observation.
-            rc = send_observ(observ)
+            rc = forward_observ(observ, app%name)
 
             if (steps == 0) then
                 ! Optimise database.
