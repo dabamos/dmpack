c1f26a90e4781b424bb9067bdce4b0a115075e10
Philipp <pengel@hs-nb.de>
2025-02-01 23:17:13 +0100

    Added sensors.

--- src/dm_sensor.f90
+++ src/dm_sensor.f90
@@ -14,7 +14,7 @@ module dm_sensor
     integer, parameter, public :: SENSOR_META_LEN = 32     !! Max. sensor meta description length.
 
     ! Sensor types.
-    integer, parameter, public :: SENSOR_TYPE_NONE    = 0  !! Unknown sensor type.
+    integer, parameter, public :: SENSOR_TYPE_NONE    = 0  !! Unknown sensor type (valid).
     integer, parameter, public :: SENSOR_TYPE_VIRTUAL = 1  !! Virtual sensor.
     integer, parameter, public :: SENSOR_TYPE_SYSTEM  = 2  !! Operating system.
     integer, parameter, public :: SENSOR_TYPE_FS      = 3  !! File system.
@@ -27,14 +27,16 @@ module dm_sensor
     integer, parameter, public :: SENSOR_TYPE_LEVEL   = 10 !! Level sensor.
     integer, parameter, public :: SENSOR_TYPE_MEMS    = 11 !! MEMS sensor.
     integer, parameter, public :: SENSOR_TYPE_CAMERA  = 12 !! IP camera or webcam.
-    integer, parameter, public :: SENSOR_TYPE_LAST    = 12 !! Never use this.
+    integer, parameter, public :: SENSOR_TYPE_MPPT    = 13 !! Maximum Power Point Tracking (MPPT) solar controller.
+    integer, parameter, public :: SENSOR_TYPE_SHUNT   = 14 !! Solar battery shunt.
+    integer, parameter, public :: SENSOR_TYPE_LAST    = 14 !! Never use this.
 
     integer, parameter, public :: SENSOR_TYPE_NAME_LEN = 7 !! Max. length of sensor type name.
 
     character(len=*), parameter, public :: SENSOR_TYPE_NAMES(SENSOR_TYPE_NONE:SENSOR_TYPE_LAST) = [ &
         character(len=SENSOR_TYPE_NAME_LEN) :: &
         'none', 'virtual', 'system', 'fs', 'process', 'network', 'multi', 'meteo', 'rts', &
-        'gnss', 'level', 'mems', 'camera' &
+        'gnss', 'level', 'mems', 'camera', 'mppt', 'shunt' &
     ] !! Array of sensor type names.
 
     type, public :: sensor_type
