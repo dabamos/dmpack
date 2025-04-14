bc3f8084e9f16dcaedc3a2ea223e5fbe502bb4e6
interkosmos <pengel@hs-nb.de>
2025-04-14 10:04:45 +0200

    Replace warning message.

--- app/dmved.f90
+++ app/dmved.f90
@@ -229,7 +229,7 @@ contains
 
                     ! Convert all frames to responses.
                     call dm_ve_frame_read(frames, responses, error=errors)
-                    if (any(dm_is_error(errors))) call logger%warning('failed to convert frame to response', error=maxval(errors))
+                    if (any(dm_is_error(errors))) call logger%debug('failed to convert frame to response', error=maxval(errors))
 
                     ! Create and forward observation.
                     call create_observ(observ, app, responses)
